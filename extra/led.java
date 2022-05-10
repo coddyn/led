62c49bef99193641bb162ccdf993957e0830a934
coddyn <97197585+coddyn@users.noreply.github.com>
2022-05-09 21:56:38 -0400

    Update led.java

--- extra/led.java
+++ extra/led.java
@@ -1,35 +1,21 @@
 package frc.robot.extra;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-
 public class led {   
-        private int[] rgb;
-        public led(int r, int g, int b){
-            rgb = new int[3];
-            rgb[0] = r;
-            rgb[1] = g;
-            rgb[2] = b;
+        private static int r;
+        private static int g;
+        private static int b;
+        public led(int red, int green, int blue){
+            r = red;
+            g = green;
+            b = blue;
         }
         public int getR(){
-            return rgb[0];
+            return r;
         }
         public int getG(){
-            return rgb[1];
+            return g;
         }
         public int getB(){
-            return rgb[2];
-        }
-        public static void changeColor(AddressableLED lights, AddressableLEDBuffer buffer, led color){
-            for(int i = 0; i < buffer.getLength(); i++){
-                buffer.setRGB(i, color.getR(), color.getG(), color.getB());
-                lights.setData(buffer);
-            }
-        }
-    
-        public static void turnOff(AddressableLED lights, AddressableLEDBuffer buffer){
-            for(int i = 0; i < buffer.getLength(); i++){
-                buffer.setRGB(i, 0, 0, 0);
-            }
+            return b;
         }
 }
