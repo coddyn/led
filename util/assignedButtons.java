1bb1b47ee334406549846741e09b060907d90de8
coddyn <97197585+coddyn@users.noreply.github.com>
2022-05-13 22:20:56 -0400

    Create assignedButtons.java

---
+++ util/assignedButtons.java
@@ -0,0 +1,8 @@
+package frc.robot.util;
+
+public class assignedButtons {
+    public final static int blueButton = allButtons.buttonA;
+    public final static int greenButton = allButtons.buttonB;
+    public final static int redButton = allButtons.buttonX;
+    public final static int rainbowButton = allButtons.buttonY;
+}
